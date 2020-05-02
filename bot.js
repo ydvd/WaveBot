6b61cfbfd0f7e5739cb83ebb33fa572bf39d4059
ydvd <ydvd1308@gmail.com>
2020-05-02 23:54:46 +0200

    Basic voice channel leaving message

--- bot.js
+++ bot.js
@@ -1,29 +1,51 @@
 const discord = require('discord.js');
 var fs = require('fs');
-const bot = new discord.Client();
-// const auth = require('auth.json');
+const client = new discord.Client();
 
-bot.on('ready', () => {
-    console.log('Logged in as ' + bot.user.tag);
+const CH_TEXT_GENERAL_ID = '669115660772573187';
+const CH_TEXT_BOT_ID = '669116268053266433';
+const CH_VOICE_GENERAL_ID = '669115660772573188';
+const CH_VOICE_DUNGEON_ID = '669115787025317919';
+const CH_VOICE_RAID_ID = '669115809901314059';
+
+client.login(JSON.parse(fs.readFileSync('auth.json')).token);
+
+/** When ready to start */
+client.on('ready', () => {
+    console.log('Logged in as ' + client.user.tag);
 });
 
-bot.on('message', msg => {
+/** Updates on messages in text channels */
+client.on('message', msg => {
     // Only reply when called
-    if(msg.content.substr(0, 4) != '!wb ') {
+    if(msg.content.substr(0, 1) != '!') {
         console.log("Irrelevant message, ignore");
         return;
     }
     else {
+
+
         console.log('Being called! Replying');
-        let message = msg.content.substr(4);
+        let message = msg.content.substr(1);
 
         if (message == 'beep') {
             msg.channel.send('boop!');
+            client.channels.fetch(CH_TEXT_BOT_ID)
+                .then(channel => channel.send("beep boop"));
+            // client.channels.z
         }
+    
+        if(message == 'ego') {
+            msg.channel.send('Ego meter currently at: 100%');
+        }
+    
+    
+    
     }
 });
 
-bot.on('voiceStateUpdate', (oldMember, newMember) => {
+/** Updates on joining/leaving voice channels */
+client.on('voiceStateUpdate', (oldMember, newMember) => {
     let newUserChannel = newMember.voiceChannel;
     let oldUserChannel = oldMember.voiceChannel;
 
@@ -36,11 +58,13 @@ bot.on('voiceStateUpdate', (oldMember, newMember) => {
             
         console.log('Someone leaves a voice channel!');
         console.log(oldMember.member.displayName);
-       // User leaves a voice channel
+        // User leaves a voice channel
    
+        client.channels.fetch(CH_TEXT_BOT_ID)
+            .then(ch => ch.send(oldMember.member.displayName + ' left voice channel ' + oldMember.channel.name));
+
      }
 
 });
 
 
-bot.login(JSON.parse(fs.readFileSync('auth.json')).token);
