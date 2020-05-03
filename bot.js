52fefe024259976f8c4e9fd5f7b541bc44eea032
ydvd <ydvd1308@gmail.com>
2020-05-03 18:21:56 +0200

    Replaced channel ID static vars with a more proper object

--- bot.js
+++ bot.js
@@ -2,20 +2,39 @@ const discord = require('discord.js');
 var fs = require('fs');
 const client = new discord.Client();
 
-//#region channel_ids
-const CH_WT_TEXT_GENERAL_ID = '669115660772573187';
-const CH_WT_TEXT_BOT_ID = '669116268053266433';
-const CH_WT_VOICE_GENERAL_ID = '669115660772573188';
-const CH_WT_VOICE_DUNGEON_ID = '669115787025317919';
-const CH_WT_VOICE_RAID_ID = '669115809901314059';
+const channel_ids = {
+    wavetest : {
+        text : {
+            general : '669115660772573187',
+            bot : '669116268053266433'
+        },
+        voice : {
+            general : '669115660772573188',
+            dungeon : '669115787025317919',
+            raid : '669115809901314059'
+        }
+    },
+    abyss_tavern : {
+        text : {
+            guild_chat : '',
+            officer_general : '699695930441597068'
+        },
+        voice : {
+            social_raid : '648227986285264919',
+            raid : '567035577980157972'
+        }
+    }
+};
 
-const CH_ABY_TEXT_OFFICER_GENERAL_ID = '699695930441597068';
-const CH_ABY_VOICE_SOCIALRAID_ID = '648227986285264919';
-const CH_ABY_VOICE_RAID_ID = '567035577980157972';
-//#endregion
-
-var activeListeners = [
-    CH_WT_VOICE_RAID_ID
+var activeTextListeners = [
+    channel_ids.wavetest.text.general,
+    channel_ids.wavetest.text.bot
+];
+var activeTextOutputs = [
+    channel_ids.wavetest.text.bot
+];
+var activeVoiceListeners = [
+    channel_ids.wavetest.voice.raid
 ];
 
 client.login(JSON.parse(fs.readFileSync('auth.json')).token);
@@ -27,28 +46,32 @@ client.on('ready', () => {
 
 /** Updates on messages in text channels */
 client.on('message', msg => {
-    // Only reply when called
-    if(msg.content.substr(0, 4) != '!wb ') {
-        return;
+    
+    // Filtering out irrelevant messages
+    if(!activeTextListeners.includes(msg.channel.id)) {
+        return; // Not in a watched channel
     }
-    // else {
+    if(msg.content.substr(0, 4) != '!wb ') {
+        return; // Not using !wb prefix
+    }
+    
+    // Replying to messages
+    console.log('Being called! Replying');
+    let message = msg.content.substr(4);
 
-        console.log('Being called! Replying');
-        let message = msg.content.substr(4);
+    if (message == 'beep') {
+        msg.channel.send('boop!');
+    }
 
-        if (message == 'beep') {
-            msg.channel.send('boop!');
-        }
+    if(message == 'ego') {
+        msg.channel.send('Ego meter currently at: 100%');
+    }
+
+    if(message == 'repo') {
+        msg.channel.send('Check out the repository here: http://github.com/ydvd/WaveBot')
+    }
     
-        if(message == 'ego') {
-            msg.channel.send('Ego meter currently at: 100%');
-        }
     
-        if(message == 'repo') {
-            msg.channel.send('Check out the repository here: http://github.com/ydvd/WaveBot')
-        }
-    
-    // }
 });
 
 /** Updates on joining/leaving voice channels */
@@ -61,17 +84,20 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
         // User Joins a voice channel
         console.log('Someone joins a voice channel!');
    
-     } else if(newUserChannel === undefined){
+     } else if(newUserChannel === undefined) {
             
         console.log('Someone leaves a voice channel!');
-        // console.log(oldMember.member.displayName);
-        console.log(oldMember.voiceChannel)
-
         // User leaves a voice channel
    
-        if(activeListeners.includes(oldMember.channel.id)) {
-            client.channels.fetch(CH_WT_TEXT_BOT_ID)
-                .then(ch => ch.send(oldMember.member.displayName + ' left voice channel ' + oldMember.channel.name));
+        if(oldMember.channel)
+        if(activeVoiceListeners.includes(oldMember.channel.id)) {
+            client.channels.fetch(channel_ids.wavetest.text.bot)
+                .then(ch => ch.send(
+                    new Date(Date.now()).toLocaleString() +
+                    ' : ' +
+                    oldMember.member.displayName + 
+                    ' left voice channel ' + 
+                    oldMember.channel.name));
         }
      }
 
