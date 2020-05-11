9d41e50ac4ba2e62407c5b4dae22ffec70d9c3c9
ydvd <ydvd1308@gmail.com>
2020-05-11 03:21:12 +0200

    First part of states and output in proper channels

--- bot.js
+++ bot.js
@@ -5,43 +5,61 @@ const client = new discord.Client();
 const channel_ids = {
     wavetest : {
         text : {
-            general : '669115660772573187',
-            bot : '669116268053266433'
+            general :           '669115660772573187',
+            bot :               '669116268053266433'
         },
         voice : {
-            general : '669115660772573188',
-            dungeon : '669115787025317919',
-            raid : '669115809901314059'
+            general :           '669115660772573188',
+            dungeon :           '669115787025317919',
+            raid :              '669115809901314059'
         }
     },
     abyss_tavern : {
         text : {
-            guild_chat : '689166130656706744',
-            officer_general : '699695930441597068'
+            guild_chat :        '689166130656706744',
+            officer_general :   '699695930441597068',
+            officer_bot :       '708400727529095181'
         },
         voice : {
-            social_raid : '648227986285264919',
-            raid : '567035577980157972'
+            social_raid :       '648227986285264919',
+            raid :              '567035577980157972'
         }
     }
 };
 
+const follower_uid = 151376963339157504;
+
+const states = {
+    OVERWATCH: 'overwatch',
+    FOLLOW: 'follow',
+    DISABLED: 'disabled'
+};
+
+var state = states.OVERWATCH;
+
 var activeTextListeners = [
     channel_ids.wavetest.text.general,
     channel_ids.wavetest.text.bot,
-    channel_ids.abyss_tavern.text.officer_general
+    channel_ids.abyss_tavern.text.officer_general,
+    channel_ids.abyss_tavern.text.officer_bot
 ];
 var activeTextOutputs = [
     channel_ids.wavetest.text.bot /* ,
     channel_ids.abyss_tavern.text.officer_general */
 ];
 var activeVoiceListeners = [
+    channel_ids.wavetest.voice.dungeon,
     channel_ids.wavetest.voice.raid,
     channel_ids.abyss_tavern.voice.social_raid,
     channel_ids.abyss_tavern.voice.raid
 ];
 
-client.login(JSON.parse(fs.readFileSync('auth.json')).token);
+try {
+    client.login(JSON.parse(fs.readFileSync('auth.json')).token);
+    
+} catch (error) {
+    console.error('Could not connect to Discord!');    
+}
 
 /** When ready to start */
 client.on('ready', () => {
@@ -63,6 +81,18 @@ client.on('message', msg => {
     console.log('Being called! Replying');
     let message = msg.content.substr(4);
 
+    // State update
+    if (message.substr(0, 4) == 'mode') {
+        changeState(msg, message.substr(5));     
+    }
+
+    if(message == 'status') {
+        msg.reply(
+            'printing current status: \n' + 
+            'Mode: ' + state
+        );
+    }
+
     if (message == 'beep') {
         msg.channel.send('boop');
     }
@@ -70,11 +100,21 @@ client.on('message', msg => {
         msg.reply('boop!');
     }
 
-    if(message == 'ego') {
+    if (message == 'ego') {
         msg.channel.send('Ego meter currently at: 100%');
     }
 
-    if(message == 'repo') {
+    if (message == 'join') {
+        client.channels.fetch(channel_ids.wavetest.voice.raid)
+            .then(raidchannel => raidchannel.join());
+    }
+
+    if(message == 'leave') {
+        client.channels.fetch(channel_ids.wavetest.voice.raid)
+            .then(raidchannel => raidchannel.leave());
+    }
+
+    if (message == 'repo') {
         msg.reply('Check out the repository here: http://github.com/ydvd/WaveBot')
     }
 
@@ -102,26 +142,63 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
     }
     else if (oldMember.channelID != null && newMember.channelID == null) {
         // User leaves channel
-        if(activeVoiceListeners.includes(oldMember.channelID)) {
-            client.channels.fetch(channel_ids.wavetest.text.bot)
-                .then(ch => ch.send(
-                    new Date(Date.now()).toLocaleString() + ' : **' +
-                    oldMember.member.displayName + '** left **' + oldMember.channel.name + '**'));
+        if ( (state == states.OVERWATCH && activeVoiceListeners.includes(oldMember.channelID)) ||
+             (state == states.FOLLOW    && false /* Should look for my connected channel */ )) {
+            for(let chid of activeTextOutputs) {
+                client.channels.fetch(chid)
+                    .then(ch => ch.send(
+                        new Date(Date.now()).toLocaleString() + ' : **' +
+                        oldMember.member.displayName + '** left **' + oldMember.channel.name + '**'));
+            }
         }
     }
     else {
         // Something else (mute/deafen/etc change)
-        if (oldMember.channelID != newMember.channelID) { // Changed channel
-            if(activeVoiceListeners.includes(oldMember.channelID)) {
-                client.channels.fetch(channel_ids.wavetest.text.bot)
-                    .then(ch => ch.send(
-                        new Date(Date.now()).toLocaleString() + ' : **' + 
-                        oldMember.member.displayName + '** moved from **' + oldMember.channel.name + 
-                        '** to **' + newMember.channel.name + '**'));
+        if (oldMember.channelID != newMember.channelID) { // Changed channel, only listening to leaving members (following lines)
+            if ( (state == states.OVERWATCH && activeVoiceListeners.includes(oldMember.channelID)) || 
+                 (state == states.FOLLOW    && false /* Should look for my connected channel */ )) {
+                for(let chid of activeTextOutputs) {
+                    client.channels.fetch(chid)
+                        .then(ch => ch.send(
+                            new Date(Date.now()).toLocaleString() + ' : **' + 
+                            oldMember.member.displayName + '** moved from **' + oldMember.channel.name + 
+                            '** to **' + newMember.channel.name + '**'));
+                }
             }
         }
     }
 });
+
+function changeState(msg, newState) {
+    let success = false;
+
+    switch(newState) {
+        case 'disabled':
+            state = states.DISABLED;
+            success = true;
+            break;
+        case 'overwatch':
+            state = states.OVERWATCH;
+            success = true;
+            break;
+        case 'follow':
+            state = states.FOLLOW;
+            success = true;
+            break;
+        default:
+            state = states.OVERWATCH;
+            break;
+    }
+    
+    if (success) {
+        console.log('State changed to ' + state);
+        msg.reply('Mode changed to ' + state);
+    }
+    else {
+        console.log('State change error: ' + newState + ' is not a valid state');
+        msg.reply(newState + ' is not a valid mode.')
+    }
+}
 
 function isVowel(c) {
     c = c.charCodeAt(c);
